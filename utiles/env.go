57144bee0e90537fefd23df9cf5db4b92729df69
kevin <13088966264@163.com>
2020-02-22 22:14:09 +0800

    update

--- utiles/env.go
+++ utiles/env.go
@@ -12,7 +12,7 @@ const (
 var (
 	Environment      string
 	Dev              = "dev"
-	ProviderEndPoint = GetEnv("CHAIN_WS_ENDPOINT", "wss://172.18.0.1:3001/")
+	ProviderEndPoint = GetEnv("CHAIN_WS_ENDPOINT", "ws://172.18.0.1:3001/")
 	// ProviderEndPoint = GetEnv("CHAIN_WS_ENDPOINT", "wss://crayfish.darwinia.network/")
 	NetworkNode      = GetEnv("NETWORK_NODE", DarwiniaNetwork)
 )
